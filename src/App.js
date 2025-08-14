46b6c324558b6901fed9c053e8c083b2cdbee723
ephraim <ephraims.818@gmail.com>
2025-08-15 04:09:55 +0600

    REACT--NET-NINJA - 03. Multiple  Components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-    const title = "Welcome to the new blog";
-    const likes = 50;
-    const person = { name: "Yoshi", age: 30 };
-
     return (
         <div className="App">
+            <Navbar />
             <div className="content">
-                <h1>{title}</h1>
-                <p>Liked {likes} times</p>
-                <p>Name: {person.name}</p>
-                <p>Age: {person.age}</p>
+                <Home />
             </div>
         </div>
     );
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <nav className="navbar">
+                <h1>The Dojo Blog</h1>
+                <div className="links">
+                    <a href="/">Home</a>
+                    <a href="/">New Blog</a>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
