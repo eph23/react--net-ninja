6dec3c400edc02671153a2ea224496948dab7d4d
ephraim <ephraims.818@gmail.com>
2025-08-16 16:11:36 +0600

    REACT--NET-NINJA - 07. Outputting Lists

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,35 @@
-import { use, useState } from "react";
+import { useState } from "react";
 
 const Home = () => {
-    const [name, setName] = useState("Mario");
-    const [age, setAge] = useState(36);
-
-    const handleClick = () => {
-        setName("Luigi");
-        setAge(age + 1);
-    };
+    const [blogs, setBlogs] = useState([
+        {
+            title: "My new website",
+            body: "lorem ipsum....",
+            author: "Mario",
+            id: 1,
+        },
+        {
+            title: "Welcome party",
+            body: "lorem ipsum....",
+            author: "Mario",
+            id: 2,
+        },
+        {
+            title: "Web dev top tips",
+            body: "lorem ipsum....",
+            author: "Mario",
+            id: 3,
+        },
+    ]);
 
     return (
         <div className="home">
-            <h2>Home page</h2>
-            <p>
-                {name} is {age} years old
-            </p>
-            <button onClick={handleClick}>Click Me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by: {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 };
