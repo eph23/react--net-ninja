f6a8f090a2dcafa3b099252d12a638a5d19102ab
ephraim <ephraims.818@gmail.com>
2025-08-18 14:25:28 +0600

    REACT--NET-NINJA - 14. Fetching Data with useEffect

--- src/Home.js
+++ src/Home.js
@@ -2,43 +2,32 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {
-            title: "My new website",
-            body: "lorem ipsum....",
-            author: "Mario",
-            id: 1,
-        },
-        {
-            title: "Welcome party",
-            body: "lorem ipsum....",
-            author: "Yoshi",
-            id: 2,
-        },
-        {
-            title: "Web dev top tips",
-            body: "lorem ipsum....",
-            author: "Mario",
-            id: 3,
-        },
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id !== id);
-        setBlogs(newBlogs);
-    };
+    // const handleDelete = (id) => {
+    //     const newBlogs = blogs.filter((blog) => blog.id !== id);
+    //     setBlogs(newBlogs);
+    // };
 
     useEffect(() => {
-        console.log(`Use Effect Run`);
+        fetch("http://localhost:8000/blogs")
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                setBlogs(data);
+            });
     }, []);
 
     return (
         <div className="home">
-            <BlogList
-                blogs={blogs}
-                title="All blogs"
-                handleDelete={handleDelete}
-            />
+            {blogs && (
+                <BlogList
+                    blogs={blogs}
+                    title="All blogs"
+                    // handleDelete={handleDelete}
+                />
+            )}
         </div>
     );
 };
