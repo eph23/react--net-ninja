aba200d3fca9f35d00eb692e83d5c43b1428a90c
ephraim <ephraims.818@gmail.com>
2025-08-19 06:59:16 +0600

    REACT--NET-NINJA - 17. Making Custom Hook

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,13 @@
 import { useState, useEffect } from "react";
+import useFetch from "./useFetch";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch("http://localhost:8000/blogss")
-                .then((res) => {
-                    if (!res.ok) {
-                        throw Error("Could Not Fetch Data");
-                    }
-                    return res.json();
-                })
-                .then((data) => {
-                    setBlogs(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch((err) => {
-                    setIsPending(false);
-                    setError(err.message);
-                });
-        }, 1000);
-    }, []);
+    const {
+        data: blogs,
+        isPending,
+        error,
+    } = useFetch("http://localhost:8000/blogs");
 
     return (
         <div className="home">
