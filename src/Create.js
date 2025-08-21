e896f9bbedd8f4bbda02e634f1f32708a81d95e5
ephraim <ephraims.818@gmail.com>
2025-08-21 20:15:10 +0600

    REACT--NET-NINJA - 27. Programatic Redirects

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
     const [author, setAuthor] = useState("Mario");
     const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -18,7 +20,9 @@ const Create = () => {
             body: JSON.stringify(blog),
         }).then(() => {
             console.log("New blog added");
-            setIsPending(true);
+            setIsPending(false);
+            // history.go(-1);
+            history.push("/");
         });
     };
 
